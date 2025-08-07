4fd1e2b8088b1def26b33ba5f814dd4633e7fcfd
Jamshid Xoliqulov <jamshidkh2004@gmail.com>
2025-08-07 17:39:23 +0000

    boshqa icon qo'y

--- src/components/layout/Logo.tsx
+++ src/components/layout/Logo.tsx
@@ -1,11 +1,11 @@
 import Link from 'next/link';
-import { BookOpenCheck } from 'lucide-react';
+import { Book } from 'lucide-react';
 import { cn } from '@/lib/utils';
 
 export default function Logo({ className }: { className?: string }) {
   return (
     <Link href="/" className={cn("flex items-center gap-2 text-xl font-bold text-primary", className)}>
-      <BookOpenCheck className="h-7 w-7" />
+      <Book className="h-7 w-7" />
       <span className="font-headline">Kitobim 2.0</span>
     </Link>
   );
