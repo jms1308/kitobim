fb8c78688e897c5a0638ab1949725d70dee4faf1
Jamshid Xoliqulov <jamshidkh2004@gmail.com>
2025-08-07 17:37:14 +0000

    change site main icon not menu icon. firebase icon change to book icon

--- src/components/layout/Logo.tsx
+++ src/components/layout/Logo.tsx
@@ -1,11 +1,11 @@
 import Link from 'next/link';
-import { BookMarked } from 'lucide-react';
+import { BookOpenCheck } from 'lucide-react';
 import { cn } from '@/lib/utils';
 
 export default function Logo({ className }: { className?: string }) {
   return (
     <Link href="/" className={cn("flex items-center gap-2 text-xl font-bold text-primary", className)}>
-      <BookMarked className="h-7 w-7" />
+      <BookOpenCheck className="h-7 w-7" />
       <span className="font-headline">Kitobim 2.0</span>
     </Link>
   );
